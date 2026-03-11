db686011878370ef6f8799658f3dfef8932f70ad
haichudo <123709389+haichudo@users.noreply.github.com>
2026-03-11 15:30:36 +0700

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,8 +1,8 @@
 const config = {
-  siteName: process.env.SITE_NAME || "Gia Phả OS",
+  siteName: process.env.SITE_NAME || "Gia Phả Nguyễn Xuân - Lâm Thao - Phú Thọ",
   exampleEmail: process.env.EXAMPLE_EMAIL || "",
   examplePassword: process.env.EXAMPLE_PASSWORD || "",
-  demoDomain: process.env.DEMO_DOMAIN || "giapha-os.homielab.com",
+  demoDomain: process.env.DEMO_DOMAIN || "https://giaphanguyenxuan-e8c1.vercel.app",
 };
 
 export default config;
