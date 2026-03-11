9e574068ceb2ef88817342f79e00a7d7843e08a4
Hai Chu Do <haichudo@gmail.com>
2026-03-11 11:46:38 +0700

    upload full project

---
+++ app/config.ts
@@ -0,0 +1,8 @@
+const config = {
+  siteName: process.env.SITE_NAME || "Gia phả họ Chu - Văn Giang - Hưng Yên",
+  exampleEmail: process.env.EXAMPLE_EMAIL || "",
+  examplePassword: process.env.EXAMPLE_PASSWORD || "",
+  demoDomain: process.env.DEMO_DOMAIN || "https://giaphahochu.vercel.app/dashboard",
+};
+
+export default config;
